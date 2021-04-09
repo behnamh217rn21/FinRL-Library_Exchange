4942962699563d11b57fd90e7fd972a6f0593f02
behnam heydari <behnamheydari.7.21@gmail.com>
2021-04-09 05:59:03 +0430

    Update training.py

--- finrl/autotrain/training.py
+++ finrl/autotrain/training.py
@@ -2,19 +2,25 @@ import pandas as pd
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
-from sklearn import preprocessing
-
 matplotlib.use("Agg")
+
+from sklearn import preprocessing
 import datetime
 
 from finrl.config import config
 from finrl.marketdata.yahoodownloader import YahooDownloader
 from finrl.preprocessing.preprocessors import FeatureEngineer
-from finrl.preprocessing.data import data_split
+from finrl.preprocessing.data import data_split, load_dataset
 from finrl.env.env_stocktrading_stoploss import StockTradingEnvStopLoss
 from finrl.model.models import DRLAgent
 from finrl.trade.backtest import backtest_plot, backtest_stats
+
 import multiprocessing
+
+# Append path for main project folder
+import sys
+sys.path.append('../../..')
+sys.path.append("./FinRL-Library_Exchange")
 
 
 def train_one():
@@ -22,7 +28,7 @@ def train_one():
     train an agent
     """
     print("==============Start Fetching Data===========")
-    df = YahooDownloader(start_date=config.START_DATE, end_date=config.END_DATE, ticker_list=config.DOW_30_TICKER, interval_period="30m").fetch_data()
+    df = load_dataset(file_name="mt4_dataset.csv")
     
     print("==============Start Feature Engineering===========")
     fe = FeatureEngineer(
@@ -43,41 +49,35 @@ def train_one():
     trade = data_split(processed, config.START_TRADE_DATE, config.END_DATE)
     
     print(StockTradingEnvStopLoss.__doc__)
-
-    information_cols = ['daily_variance', 'change', 'log_volume', 'close', 'day', 
-                        'macd', 'rsi_30', 'cci_30', 'dx_30']
+    information_cols = ["daily_variance", "change", "log_volume", "close", 
+                        "macd", "boll_ub", "boll_lb", "rsi_30", "cci_30", "dx_30", "close_30_sma", "close_60_sma"]
     env_train_kwargs = {
-        initial_amount = 1e6,hmax = 5000, 
-        turbulence_threshold = None, 
-        currency='$',
-        buy_cost_pct=3e-3,
-        sell_cost_pct=3e-3,
-        cash_penalty_proportion=0.2,
-        cache_indicator_data=True,
-        daily_information_cols = information_cols, 
-        print_verbosity = 500, 
-        random_start = True
+        'initial_amount': 10000,
+        'hmax': 100, 
+        'cache_indicator_data': True,
+        'cash_penalty_proportion': 0.2, 
+        'daily_information_cols': information_cols, 
+        'print_verbosity': 500, 
+        'random_start': True ,
+        'discrete_actions': True
     }
-    e_train_gym = StockTradingEnvStopLoss(df=train, **env_kwargs)
+    e_train_gym = StockTradingEnvStopLoss(df = train, **env_train_kwargs)
     
     env_trade_kwargs = {
-        initial_amount = 1e6,hmax = 5000, 
-        turbulence_threshold = None, 
-        currency='$',
-        buy_cost_pct=3e-3,
-        sell_cost_pct=3e-3,
-        cash_penalty_proportion=0.2,
-        cache_indicator_data=True,
-        daily_information_cols = information_cols, 
-        print_verbosity = 500, 
-        random_start = True
+        'initial_amount': 10000,
+        'hmax': 100, 
+        'cache_indicator_data': True,
+        'cash_penalty_proportion': 0.2, 
+        'daily_information_cols': information_cols, 
+        'print_verbosity': 500, 
+        'random_start': False,
+        'discrete_actions': True
     }
-    e_trade_gym = StockTradingEnvStopLoss(df=trade, turbulence_threshold=250, **env_kwargs)
+    e_trade_gym = StockTradingEnvStopLoss(df = trade, **env_trade_kwargs)
     
     # for this example, let's do multiprocessing with n_cores-2
     n_cores = multiprocessing.cpu_count() - 2
-    print(f"using {n_cores} cores")
-    
+    print(f"using {n_cores} cores")   
     # this is our training env. It allows multiprocessing
     env_train, _ = e_train_gym.get_multiproc_env(n = n_cores)
     #env_train, _ = e_train_gym.get_sb_env()
@@ -87,8 +87,6 @@ def train_one():
 
     agent = DRLAgent(env=env_train)
     print("==============Model Training===========")
-    now = datetime.datetime.now().strftime("%Y%m%d-%Hh%M")
-
     ddpg_params ={'actor_lr': 5e-06,
                   'critic_lr': 5e-06,
                   'gamma': 0.99,
@@ -106,7 +104,7 @@ def train_one():
         model=model, tb_log_name="ddpg", total_timesteps=80000, log_interval=1
     )
 
-    #model.save("trained_models/DDPG.model")
+    model.save("trained_models/DDPG.model")
     
     logs_base_dir = f"{config.TENSORBOARD_LOG_DIR}/{model_name}"
     os.makedirs(logs_base_dir, exist_ok=True)
@@ -118,6 +116,7 @@ def train_one():
                                                            environment = e_trade_gym
                                                           )
     
+    now = datetime.datetime.now().strftime("%Y%m%d-%Hh%M")
     df_account_value.to_csv("./" + config.RESULTS_DIR + "/df_account_value_" + now + ".csv")
     df_actions.to_csv("./" + config.RESULTS_DIR + "/df_actions_" + now + ".csv")
 
@@ -125,14 +124,3 @@ def train_one():
     perf_stats_all = backtest_stats(account_value=df_account_value, value_col_name = 'total_assets')
     perf_stats_all = pd.DataFrame(perf_stats_all)
     perf_stats_all.to_csv("./" + config.RESULTS_DIR + "/perf_stats_all_" + now + ".csv")
-    
-    
-    print("==============Compare to DJIA===========")
-    %matplotlib inline
-    # S&P 500: ^GSPC
-    # Dow Jones Index: ^DJI
-    # NASDAQ 100: ^NDX
-    backtest_plot(df_account_value, 
-                  baseline_ticker = '^DJI', 
-                  baseline_start = config.START_TRADE_DATE,
-                  baseline_end = config.END_DATE, value_col_name = 'total_assets')
