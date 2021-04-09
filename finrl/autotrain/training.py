62fddbb8b392c8fbf46389404a5bde1c8fe9f9f1
behnam heydari <behnamheydari.7.21@gmail.com>
2021-04-09 16:05:09 +0430

    Update training.py

--- finrl/autotrain/training.py
+++ finrl/autotrain/training.py
@@ -22,8 +22,9 @@ import multiprocessing
 
 # Append path for main project folder
 import sys
-sys.path.append('../')
+sys.path.append('../../')
 sys.path.append("./FinRL-Library_Exchange")
+
 
 """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
