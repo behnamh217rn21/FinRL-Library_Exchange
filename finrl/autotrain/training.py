fbd2616a7b05618a687a793ec395c33e19dec5f7
behnam heydari <behnamheydari.7.21@gmail.com>
2021-04-16 19:33:27 +0430

    Update training.py

--- finrl/autotrain/training.py
+++ finrl/autotrain/training.py
@@ -61,8 +61,8 @@ def main():
     print("****Environment Document****")
     print(StockTradingEnvStopLoss.__doc__)
     
-    information_cols = ["close", "macd", "boll_ub", "boll_lb", "rsi_30", "cci_30", "dx_30", "close_30_sma", "close_60_sma", "log_volume", "change", "daily_variance"]
     print("****Build Train Environment****")
+    information_cols = ["close", "macd", "boll_ub", "boll_lb", "rsi_30", "cci_30", "dx_30", "close_30_sma", "close_60_sma", "log_volume", "change", "daily_variance"]
     env_train_kwargs = {'initial_amount': 20000,
                         'hmax': 100, 
                         'cache_indicator_data': True,
