a3f78a5c5b2680b4dce991bad9cd20a22fc69328
behnam heydari <behnamheydari.7.21@gmail.com>
2021-04-16 19:36:33 +0430

    Update training_v1.py

--- finrl/autotrain/training_v1.py
+++ finrl/autotrain/training_v1.py
@@ -34,19 +34,20 @@ def Data_Downloader(_symbols):
 def main():
     """
     train an agent
-    """
-    print("==============Start Fetching Data===========")
+    """ 
+    print("==============Start Trading===========")
+    print("****Start Fetching Data****")
     #df = YahooDownloader(start_date=config.START_DATE, end_date=config.END_DATE, ticker_list=ticker, interval_period="30m").fetch_data()   
     with open("./" + config.DATA_SAVE_DIR + "/symbols.txt", "r") as file:
         _symbols = eval(file.readline())
-    _symbols_list = []
+    _symbols_i1 = []
     for i in range(0, len(_symbols)):
-        _symbols_list.append(_symbols[i][1])
+        _symbols_i1.append(_symbols[i][1])
     Data_Downloader(_symbols_list)
-    df = load_dataset(file_name="mt4_dataset.csv")
+    df = load_dataset(file_name="data.csv")
     print(df.head())
 
-    print("==============Start Feature Engineering===========")
+    print("****Start Feature Engineering****")
     fe = FeatureEngineer(
         use_technical_indicator=True,
         tech_indicator_list=config.TECHNICAL_INDICATORS_LIST,
@@ -54,68 +55,66 @@ def main():
         user_defined_feature=False,
     )
     processed = fe.preprocess_data(df)
+    np.seterr(divide = 'ignore')
+    processed['log_volume'] = np.where((processed.volume*processed.close)>0, np.log(processed.volume*processed.close), 0)
     processed['change'] = (processed.close-processed.open)/processed.close
-    processed['log_volume'] = np.log(processed.volume*processed.close)
     processed['daily_variance'] = (processed.high-processed.low)/processed.close
+    print(processed.head())
     processed.to_csv("./" + config.DATA_SAVE_DIR + "/Dataframe/data_df.csv")
         
+    print("****Training & Trading data split****")
     # Training data split
-    train = data_split(processed, config.START_DATE, config.START_TRADE_DATE)
+    train = data_split(processed, config.START_DATE, config.END_DATE)
+    print("train dataset length: {}".format(str(len(train)))
 
+    print("****Environment Document****")
     print(StockTradingEnvStopLoss.__doc__)
-    information_cols = ["daily_variance", "change", "log_volume", "close", 
-                        "macd", "boll_ub", "boll_lb", "rsi_30", "cci_30", "dx_30", "close_30_sma", "close_60_sma"]
-    env_train_kwargs = {
-        'initial_amount': 10000,
-        'hmax': 100, 
-        'cache_indicator_data': True,
-        'cash_penalty_proportion': 0.2, 
-        'daily_information_cols': information_cols, 
-        'print_verbosity': 500, 
-        'random_start': True ,
-        'discrete_actions': True
-    }
-    e_train_gym = StockTradingEnvStopLoss(df = train, **env_train_kwargs)
     
+    print("****Build Train Environment****")
+    file = open("./" + config.DATA_SAVE_DIR + "/balance.txt","r+") 
+    initial_amount = file.read()
+    file.close()
+    information_cols = ["close", "macd", "boll_ub", "boll_lb", "rsi_30", "cci_30", "dx_30", "close_30_sma", "close_60_sma", "log_volume", "change", "daily_variance"]
+    env_trade_kwargs = {'initial_amount': initial_amount,
+                        'hmax': 100, 
+                        'cache_indicator_data': True,
+                        'daily_information_cols': information_cols,
+                        'print_verbosity': 500, 
+                        'discrete_actions': True}
+    e_train_gym = StockTradingEnvStopLoss(df = train, **env_train_kwargs)
     # for this example, let's do multiprocessing with n_cores-2
     n_cores = multiprocessing.cpu_count() - 2
     print(f"using {n_cores} cores")  
     # this is our training env. It allows multiprocessing
-    #env_train, _ = e_train_gym.get_multiproc_env(n = n_cores)
-    env_train, _ = e_train_gym.get_sb_env()
-
+    env_train, _ = e_train_gym.get_multiproc_env(n = n_cores)
+    #env_train, _ = e_train_gym.get_sb_env()
+    
+    print("****Implement DRL Algorithms****")
     agent = DRLAgent(env=env_train)
-    print("==============Model Training===========")
-    ddpg_params ={'actor_lr': 5e-06,
-                  'critic_lr': 5e-06,
-                  'gamma': 0.99,
-                  'batch_size': 1024,
-                  'eval_env': env_trade,
-                  'nb_eval_steps': 50
-                 }
+    ddpg_params ={"actor_lr": 5e-06,
+                  "critic_lr": 5e-06,
+                  "gamma": 0.99,
+                  "batch_size": 1024}  
     
-    policy_kwargs = {
-        "net_arch": ["lstm", "lstm", dict(pi=[dict(lstm_L1=24, dropout_L2=0.2, lstm_L3=24, dropout_L4=0.2)], \
-                                          vf=[dict(dense_L1=64, dense_L2=16)])],
-        "n_lstm": 10
-    }
+    policy_kwargs = {"net_arch": ["lstm", "lstm", dict(pi=[dict(lstm_L1=24, dropout_L2=0.2, lstm_L3=24, dropout_L4=0.2)], \
+                                                       vf=[dict(dense_L1=64, dense_L2=16)])],
+                     "n_lstm": 10}
     
-    model_name = "ddpg"
-    model = agent.get_model(model_name,
+    model = agent.get_model("ddpg",
+                            "eval_env": env_trade,
                             policy="LstmLstmPolicy",
                             model_kwargs = ddpg_params,
-                            policy_kwargs = policy_kwargs,
-                            verbose = 0
-                           )
+                            policy_kwargs = policy_kwargs,   
+                            verbose = 0)
         
+    print("****Train_Model****")
     trained_ddpg = agent.train_model(model=model, 
-                                     tb_log_name="ddpg", 
-                                     total_timesteps=80000,
-                                     log_interval=1
-                                    )
-    model.save("./" + config.TRAINED_MODEL_DIR + "/DDPG.model")
+                                     total_timesteps=32600000,
+                                     log_interval=1)
+    
+    print("****Model Saving****")
+    trained_ddpg.save("./" + config.TRAINED_MODEL_DIR + "/DDPG.model")
     
     
 if __name__ == "__main__":
     main()
-    
