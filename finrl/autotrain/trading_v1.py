e763452d357bc8a643a03703bd8fd7cd2e0723a1
behnam heydari <behnamheydari.7.21@gmail.com>
2021-04-18 19:52:35 +0430

    Update trading_v1.py

--- finrl/autotrain/trading_v1.py
+++ finrl/autotrain/trading_v1.py
@@ -39,20 +39,20 @@ def rates_subscriptions(_symbols):
                   
 def main():
     print("==============Start Trading===========")
-    trained_ddpg = model.load(".\\" + config.TRAINED_MODEL_DIR + "\\DDPG.model")
+    DDPG_model = model.load("./" + config.TRAINED_MODEL_DIR + "/DDPG.model")
     
     print("****Environment Document****")
     print(StockTradingEnvStopLoss_online.__doc__)
     
     print("****rates subscriptions process****")
-    with open(".\\" + config.DATA_SAVE_DIR + "\\symbols.txt", "r") as file:
+    with open("./" + config.DATA_SAVE_DIR + "/symbols.txt", "r") as file:
         _symbols = eval(file.readline())
     process = multiprocessing.Process(target=rates_subscriptions(), args=(_symbols,))
     process.start()
     sleep(60)
 
     print("****Build Trade Environment****")
-    file = open(".\\" + config.DATA_SAVE_DIR + "\\balance.txt","r+") 
+    file = open("./" + config.DATA_SAVE_DIR + "/balance.txt","r+") 
     initial_amount = file.read()
     initial_amount = float(initial_amount)
     file.close()
@@ -64,24 +64,22 @@ def main():
                         'random_start': False,
                         'discrete_actions': True}
     e_trade_gym = StockTradingEnvStopLoss_online(**env_trade_kwargs)
-    # this is our observation environment. It allows full diagnostics
-    #env_trade, _ = e_trade_gym.get_sb_env()
     
     print("==============Start Trading===========")
     print("****Model Prediction****")
-    df_account_value, df_actions = DRLAgent.DRL_prediction(model=trained_ddpg, 
+    df_account_value, df_actions = DRLAgent.DRL_prediction(model=DDPG_model, 
                                                            environment = e_trade_gym)
     
     print("****Prediction Resault Saving****")
     now = datetime.datetime.now().strftime("%Y%m%d-%Hh%M")
-    df_account_value.to_csv(".\\" + config.RESULTS_DIR + "\\_df_account_value" + now + ".csv")
-    df_actions.to_csv(".\\" + config.RESULTS_DIR + "\\_df_actions" + now + ".csv")
+    df_account_value.to_csv("./" + config.RESULTS_DIR + "/_df_account_value" + now + ".csv")
+    df_actions.to_csv("./" + config.RESULTS_DIR + "/_df_actions" + now + ".csv")
     
     print("****Get Backtest Results****")
     perf_stats_all = backtest_stats(account_value=df_account_value, value_col_name = 'total_assets')
     perf_stats_all = pd.DataFrame(perf_stats_all)
-    perf_stats_all.to_csv(".\\" + config.RESULTS_DIR + "\\_perf_stats_all" + now + ".csv")
-
+    perf_stats_all.to_csv("./" + config.RESULTS_DIR + "/_perf_stats_all" + now + ".csv")
     
+
 if __name__ == "__main__":
-    main()  
+    main()
