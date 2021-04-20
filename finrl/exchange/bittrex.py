98cd8a66d89e3f277c0b5b0137e698d74090174f
behnam heydari <behnamheydari.7.21@gmail.com>
2021-04-20 15:25:27 +0430

    Update bittrex.py

--- finrl/exchange/bittrex.py
+++ finrl/exchange/bittrex.py
@@ -4,7 +4,6 @@ from typing import Dict
 
 from finrl.exchange import Exchange
 
-
 logger = logging.getLogger(__name__)
 
 
@@ -12,12 +11,9 @@ class Bittrex(Exchange):
     """
     Bittrex exchange class. Contains adjustments needed for Freqtrade to work
     with this exchange.
-
     Please note that this exchange is not included in the list of exchanges
     officially supported by the Freqtrade development team. So some features
     may still not work as expected.
     """
-
-    _ft_has: Dict = {
-        "l2_limit_range": [1, 25, 500],
-    }
+    _ft_has: Dict = {"l2_limit_range": [1, 25, 500],
+                    }
