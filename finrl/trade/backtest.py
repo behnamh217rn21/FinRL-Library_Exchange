29c49bf85438415bbe1b52a0b822a8204738c2c1
behnam heydari <behnamheydari.7.21@gmail.com>
2021-04-20 10:37:59 +0430

    Update backtest.py

--- finrl/trade/backtest.py
+++ finrl/trade/backtest.py
@@ -22,42 +22,33 @@ def get_daily_return(df, value_col_name="account_value"):
 
 def backtest_stats(account_value, value_col_name="account_value"):
     dr_test = get_daily_return(account_value, value_col_name=value_col_name)
-    perf_stats_all = timeseries.perf_stats(
-        returns=dr_test,
-        positions=None,
-        transactions=None,
-        turnover_denom="AGB",
-    )
+    perf_stats_all = timeseries.perf_stats(returns=dr_test,
+                                           positions=None,
+                                           transactions=None,
+                                           turnover_denom="AGB",
+                                          )
     print(perf_stats_all)
     return perf_stats_all
 
 
-def backtest_plot(
-    account_value,
-    baseline_start=config.START_TRADE_DATE,
-    baseline_end=config.END_DATE,
-    baseline_ticker="^DJI",
-    value_col_name="account_value",
-):
-
+def backtest_plot(account_value,
+                  baseline_start=config.START_TRADE_DATE,
+                  baseline_end=config.END_DATE,
+                  baseline_ticker="^DJI",
+                  value_col_name="account_value",
+                 ):
     df = deepcopy(account_value)
     test_returns = get_daily_return(df, value_col_name=value_col_name)
 
-    baseline_df = get_baseline(
-        ticker=baseline_ticker, start=baseline_start, end=baseline_end
-    )
+    baseline_df = get_baseline(ticker=baseline_ticker, start=baseline_start, end=baseline_end)
 
     baseline_returns = get_daily_return(baseline_df, value_col_name="close")
     with pyfolio.plotting.plotting_context(font_scale=1.1):
-        pyfolio.create_full_tear_sheet(
-            returns=test_returns, benchmark_rets=baseline_returns, set_context=False
-        )
+        pyfolio.create_full_tear_sheet(returns=test_returns, benchmark_rets=baseline_returns, set_context=False)
 
 
 def get_baseline(ticker, start, end):
-    dji = YahooDownloader(
-        start_date=start, end_date=end, ticker_list=[ticker]
-    ).fetch_data()
+    dji = YahooDownloader(start_date=start, end_date=end, ticker_list=[ticker]).fetch_data()
     return dji
 
 
