4d4cedbee994660c9de72f42c20b2480140eeb99
behnam heydari <behnamheydari.7.21@gmail.com>
2021-04-20 13:54:33 +0430

    Update dataprovider.py

--- finrl/data/dataprovider.py
+++ finrl/data/dataprovider.py
@@ -16,7 +16,6 @@ from finrl.exceptions import ExchangeError, OperationalException
 from finrl.exchange import Exchange
 from finrl.state import RunMode
 
-
 logger = logging.getLogger(__name__)
 
 
@@ -27,34 +26,33 @@ class DataProvider:
         self._pairlists = pairlists
         self.__cached_pairs: Dict[PairWithTimeframe, Tuple[DataFrame, datetime]] = {}
 
+            
     def _set_cached_df(self, pair: str, timeframe: str, dataframe: DataFrame) -> None:
         """
         Store cached Dataframe.
         Using private method as this should never be used by a user
         (but the class is exposed via `self.dp` to the strategy)
-        
         param pair: 
-            pair to get the data for
-            
+            pair to get the data for        
         param timeframe: 
-            Timeframe to get data for
-            
+            Timeframe to get data for          
         param dataframe: 
             analyzed dataframe
         """
         self.__cached_pairs[(pair, timeframe)] = (dataframe, datetime.now(timezone.utc))
 
+        
     def add_pairlisthandler(self, pairlists) -> None:
         """
         Allow adding pairlisthandler after initialization
         """
         self._pairlists = pairlists
 
-    def refresh(
-        self,
-        pairlist: ListPairsWithTimeframes,
-        helping_pairs: ListPairsWithTimeframes = None,
-    ) -> None:
+        
+    def refresh(self,
+                pairlist: ListPairsWithTimeframes,
+                helping_pairs: ListPairsWithTimeframes = None,
+               ) -> None:
         """
         Refresh data, called with each cycle
         """
@@ -63,6 +61,7 @@ class DataProvider:
         else:
             self._exchange.refresh_latest_ohlcv(pairlist)
 
+            
     @property
     def available_pairs(self) -> ListPairsWithTimeframes:
         """
@@ -71,56 +70,49 @@ class DataProvider:
         """
         return list(self._exchange._klines.keys())
 
+    
     def ohlcv(self, pair: str, timeframe: str = None, copy: bool = True) -> DataFrame:
         """
         Get candle (OHLCV) data for the given pair as DataFrame
         Please use the `available_pairs` method to verify which pairs are currently cached.
-        
         param pair: 
-            pair to get the data for
-            
+            pair to get the data for     
         param timeframe: 
-            Timeframe to get data for
-            
+            Timeframe to get data for     
         param copy: 
             copy dataframe before returning if True.
             Use False only for read-only operations (where the dataframe is not modified)
         """
         if self.runmode in (RunMode.DRY_RUN, RunMode.LIVE):
-            return self._exchange.klines(
-                (pair, timeframe or self._config["timeframe"]), copy=copy
-            )
+            return self._exchange.klines((pair, timeframe or self._config["timeframe"]), 
+                                         copy=copy)
         else:
             return DataFrame()
 
+        
     def historic_ohlcv(self, pair: str, timeframe: str = None) -> DataFrame:
         """
         Get stored historical candle (OHLCV) data
-        
         param pair: 
-            pair to get the data for
-            
+            pair to get the data for        
         param timeframe: 
             timeframe to get data for
         """
-        return load_pair_history(
-            pair=pair,
-            timeframe=timeframe or self._config["timeframe"],
-            datadir=self._config["datadir"],
-            data_format=self._config.get("dataformat_ohlcv", "json"),
-        )
+        return load_pair_history(pair=pair,
+                                 timeframe=timeframe or self._config["timeframe"],
+                                 datadir=self._config["datadir"],
+                                 data_format=self._config.get("dataformat_ohlcv", "json"),
+                                )
 
+    
     def get_pair_dataframe(self, pair: str, timeframe: str = None) -> DataFrame:
         """
         Return pair candle (OHLCV) data, either live or cached historical -- depending
         on the runmode.
-        
         param pair: 
-            pair to get the data for
-            
+            pair to get the data for      
         param timeframe: 
-            timeframe to get data for
-            
+            timeframe to get data for     
         return: 
             Dataframe for this pair
         """
@@ -130,20 +122,18 @@ class DataProvider:
         else:
             # Get historical OHLCV data (cached on disk).
             data = self.historic_ohlcv(pair=pair, timeframe=timeframe)
+            
         if len(data) == 0:
             logger.warning(f"No data found for ({pair}, {timeframe}).")
         return data
 
-    def get_analyzed_dataframe(
-        self, pair: str, timeframe: str
-    ) -> Tuple[DataFrame, datetime]:
+    
+    def get_analyzed_dataframe(self, pair: str, timeframe: str) -> Tuple[DataFrame, datetime]:
         """
         param pair: 
             pair to get the data for
-            
         param timeframe: 
-            timeframe to get data for
-            
+            timeframe to get data for     
         return: 
             Tuple of (Analyzed Dataframe, lastrefreshed) for the requested 
             pair / timeframe combination.
@@ -152,28 +142,25 @@ class DataProvider:
         if (pair, timeframe) in self.__cached_pairs:
             return self.__cached_pairs[(pair, timeframe)]
         else:
-
             return (DataFrame(), datetime.fromtimestamp(0, tz=timezone.utc))
 
+        
     def market(self, pair: str) -> Optional[Dict[str, Any]]:
         """
         Return market data for the pair
-        
         param pair: 
-            Pair to get the data for
-            
+            Pair to get the data for    
         return: 
             Market data dict from ccxt or None if market info is not available for the pair
         """
         return self._exchange.markets.get(pair)
 
+    
     def ticker(self, pair: str):
         """
         Return last ticker data from exchange
-        
         param pair: 
-            Pair to get the data for
-            
+            Pair to get the data for  
         return: 
             Ticker dict from exchange or empty dict if ticker is not available for the pair
         """
@@ -182,22 +169,21 @@ class DataProvider:
         except ExchangeError:
             return {}
 
+        
     def orderbook(self, pair: str, maximum: int) -> Dict[str, List]:
         """
         Fetch latest l2 orderbook data
         Warning: Does a network request - so use with common sense.
-        
         param pair: 
             pair to get the data for
-            
         param maximum: 
-            Maximum number of orderbook entries to query
-            
+            Maximum number of orderbook entries to query 
         return: 
             dict including bids/asks with a total of `maximum` entries.
         """
         return self._exchange.fetch_l2_order_book(pair, maximum)
 
+    
     @property
     def runmode(self) -> RunMode:
         """
@@ -206,20 +192,16 @@ class DataProvider:
         """
         return RunMode(self._config.get("runmode", RunMode.OTHER))
 
+    
     def current_whitelist(self) -> List[str]:
         """
         fetch latest available whitelist.
-
         Useful when you have a large whitelist and need to call each pair as an informative pair.
         As available pairs does not show whitelist until after informative pairs have been cached.
-        
         return: 
             list of pairs in whitelist
         """
-
         if self._pairlists:
             return self._pairlists.whitelist
         else:
-            raise OperationalException(
-                "Dataprovider was not initialized with a pairlist provider."
-            )
+            raise OperationalException("Dataprovider was not initialized with a pairlist provider.")
