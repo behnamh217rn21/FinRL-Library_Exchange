f9aca1340e8ad19b9d6febbfc2c07666d48d881b
behnam heydari <behnamheydari.7.21@gmail.com>
2021-04-20 17:45:54 +0430

    Update main.py

--- main.py
+++ main.py
@@ -10,14 +10,14 @@ from finrl.config import config
 
 def build_parser():
     parser = ArgumentParser()
-    parser.add_argument(
-        "--mode",
-        dest="mode",
-        help="start mode, train, download_data" " backtest",
-        metavar="MODE",
-        default="train",
-    )
+    parser.add_argument("--mode",
+                        dest="mode",
+                        help="start mode, train, download_data" " backtest",
+                        metavar="MODE",
+                        default="train",
+                       )
     return parser
+
 
 def main():
     parser = build_parser()
@@ -26,19 +26,17 @@ def main():
         os.makedirs("./" + config.TRAINED_MODEL_DIR)
     if not os.path.exists("./" + config.RESULTS_DIR):
         os.makedirs("./" + config.RESULTS_DIR)
-    if not os.path.exists("./" + config.DATA_SAVE_DIR):
-        os.makedirs("./" + config.DATA_SAVE_DIR)
     if not os.path.exists("./" + config.TENSORBOARD_LOG_DIR):
         os.makedirs("./" + config.TENSORBOARD_LOG_DIR)
+    if not os.path.exists("./" + config.DATA_SAVE_DIR):
+        os.makedirs("./" + config.DATA_SAVE_DIR)
 
     if options.mode == "train":
         import finrl.autotrain.training
-
         finrl.autotrain.training.train_one()
 
     elif options.mode == "download_data":
         from finrl.marketdata.yahoodownloader import YahooDownloader
-
         df = YahooDownloader(start_date=config.START_DATE,
                              end_date=config.END_DATE,
                              ticker_list=config.DOW_30_TICKER).fetch_data()
