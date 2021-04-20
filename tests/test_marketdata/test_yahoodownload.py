6e432892b306d31998fa27747b80736cb1fef6a9
behnam heydari <behnamheydari.7.21@gmail.com>
2021-04-20 17:48:51 +0430

    Update test_yahoodownload.py

--- tests/test_marketdata/test_yahoodownload.py
+++ tests/test_marketdata/test_yahoodownload.py
@@ -6,10 +6,7 @@ import pandas as pd
 class TestDownloader(unittest.TestCase):
     def setUp(cls):
         cls.ticker_list = ["AAPL", "GOOG"]
-
+        
     def test_download(self):
-        df = YahooDownloader(
-            start_date="2019-01-01", end_date="2019-02-01", ticker_list=self.ticker_list
-        ).fetch_data()
-
+        df = YahooDownloader(start_date="2019-01-01", end_date="2019-02-01", ticker_list=self.ticker_list).fetch_data()
         self.assertIsInstance(df, pd.DataFrame)
