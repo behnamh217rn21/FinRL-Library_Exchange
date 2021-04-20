5ffdb38721eef88fd6a2ef53b2ba4a75ef3dfe99
behnam heydari <behnamheydari.7.21@gmail.com>
2021-04-20 13:47:36 +0430

    Update timerange.py

--- finrl/config/timerange.py
+++ finrl/config/timerange.py
@@ -7,7 +7,6 @@ from typing import Optional
 
 import arrow
 
-
 logger = logging.getLogger(__name__)
 
 
@@ -17,48 +16,44 @@ class TimeRange:
     [start/stop]type defines if [start/stop]ts shall be used.
     if *type is None, don't use corresponding startvalue.
     """
-
-    def __init__(self, starttype: Optional[str] = None, stoptype: Optional[str] = None,
+    def __init__(self, starttype: Optional[str] = None, 
+                 stoptype: Optional[str] = None,
                  startts: int = 0, stopts: int = 0):
-
         self.starttype: Optional[str] = starttype
         self.stoptype: Optional[str] = stoptype
         self.startts: int = startts
         self.stopts: int = stopts
 
+            
     def __eq__(self, other):
         """Override the default Equals behavior"""
         return (self.starttype == other.starttype and self.stoptype == other.stoptype
                 and self.startts == other.startts and self.stopts == other.stopts)
 
+    
     def subtract_start(self, seconds: int) -> None:
         """
         Subtracts <seconds> from startts if startts is set.
-        
         param seconds: 
-            Seconds to subtract from starttime
-            
+            Seconds to subtract from starttime       
         return: 
             None (Modifies the object in place)
         """
         if self.startts:
             self.startts = self.startts - seconds
 
+            
     def adjust_start_if_necessary(self, timeframe_secs: int, startup_candles: int,
                                   min_date: arrow.Arrow) -> None:
         """
         Adjust startts by <startup_candles> candles.
         Applies only if no startup-candles have been available.
-        
         param timeframe_secs: 
-            Timeframe in seconds e.g. `timeframe_to_seconds('5m')`
-            
+            Timeframe in seconds e.g. `timeframe_to_seconds('5m')`    
         param startup_candles: 
-            Number of candles to move start-date forward
-            
+            Number of candles to move start-date forward   
         param min_date: 
-            Minimum data date loaded. Key kriterium to decide if start-time has to be moved
-            
+            Minimum data date loaded. Key kriterium to decide if start-time has to be moved   
         return: 
             None (Modifies the object in place)
         """
@@ -70,19 +65,19 @@ class TimeRange:
             self.startts = (min_date.int_timestamp + timeframe_secs * startup_candles)
             self.starttype = 'date'
 
+            
     @staticmethod
     def parse_timerange(text: Optional[str]) -> 'TimeRange':
         """
         Parse the value of the argument --timerange to determine what is the range desired
-        
         param text: 
-            value from --timerange
-            
+            value from --timerange 
         return: 
             Start and End range period
         """
         if text is None:
             return TimeRange(None, None, 0, 0)
+        
         syntax = [(r'^-(\d{8})$', (None, 'date')),
                   (r'^(\d{8})-$', ('date', None)),
                   (r'^(\d{8})-(\d{8})$', ('date', 'date')),
@@ -99,8 +94,7 @@ class TimeRange:
             if match:  # Regex has matched
                 rvals = match.groups()
                 index = 0
-                start: int = 0
-                stop: int = 0
+                start: int = 0; stop: int = 0
                 if stype[0]:
                     starts = rvals[index]
                     if stype[0] == 'date' and len(starts) == 8:
@@ -118,5 +112,6 @@ class TimeRange:
                         stop = int(stops) // 1000
                     else:
                         stop = int(stops)
+                        
                 return TimeRange(stype[0], stype[1], start, stop)
         raise Exception('Incorrect syntax for timerange "%s"' % text)
