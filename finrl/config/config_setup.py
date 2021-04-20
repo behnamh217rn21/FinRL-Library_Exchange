864c6f30dd7b4102646ca722e1c3f71067f63a9c
behnam heydari <behnamheydari.7.21@gmail.com>
2021-04-20 13:32:33 +0430

    Update config_setup.py

--- finrl/config/config_setup.py
+++ finrl/config/config_setup.py
@@ -13,15 +13,14 @@ logger = logging.getLogger(__name__)
 def setup_utils_configuration(args: Dict[str, Any], method: RunMode) -> Dict[str, Any]:
     """
     Prepare the configuration for utils subcommands
-    
     param args: 
         Cli args from Arguments()
-        
     return: 
         Configuration
     """
     configuration = Configuration(args, method)
     config = configuration.get_config()
+    
     # Ensure we do not use Exchange credentials
     remove_credentials(config)
     validate_config_consistency(config)
