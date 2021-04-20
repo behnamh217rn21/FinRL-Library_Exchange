8be28b6cd9de65659f2352862b28850b17389435
behnam heydari <behnamheydari.7.21@gmail.com>
2021-04-20 16:04:30 +0430

    Update hdf5datahandler.py

--- finrl/data/history/hdf5datahandler.py
+++ finrl/data/history/hdf5datahandler.py
@@ -13,14 +13,11 @@ from finrl.constants import (DEFAULT_DATAFRAME_COLUMNS, DEFAULT_TRADES_COLUMNS,
 
 from .idatahandler import IDataHandler
 
-
 logger = logging.getLogger(__name__)
 
 
 class HDF5DataHandler(IDataHandler):
-
     _columns = DEFAULT_DATAFRAME_COLUMNS
-
     @classmethod
     def ohlcv_get_available_data(cls, datadir: Path) -> ListPairsWithTimeframes:
         """
@@ -33,6 +30,7 @@ class HDF5DataHandler(IDataHandler):
         return [(match[1].replace('_', '/'), match[2]) for match in _tmp
                 if match and len(match.groups()) > 1]
 
+      
     @classmethod
     def ohlcv_get_pairs(cls, datadir: Path, timeframe: str) -> List[str]:
         """
@@ -42,12 +40,12 @@ class HDF5DataHandler(IDataHandler):
         :param timeframe: Timeframe to search pairs for
         :return: List of Pairs
         """
-
         _tmp = [re.search(r'^(\S+)(?=\-' + timeframe + '.h5)', p.name)
                 for p in datadir.glob(f"*{timeframe}.h5")]
         # Check if regex found something and only return these results
         return [match[0].replace('_', '/') for match in _tmp if match]
 
+      
     def ohlcv_store(self, pair: str, timeframe: str, data: pd.DataFrame) -> None:
         """
         Store data in hdf5 file.
@@ -58,14 +56,12 @@ class HDF5DataHandler(IDataHandler):
         """
         key = self._pair_ohlcv_key(pair, timeframe)
         _data = data.copy()
-
         filename = self._pair_data_filename(self._datadir, pair, timeframe)
-
         ds = pd.HDFStore(filename, mode='a', complevel=9, complib='blosc')
         ds.put(key, _data.loc[:, self._columns], format='table', data_columns=['date'])
-
         ds.close()
 
+        
     def _ohlcv_load(self, pair: str, timeframe: str,
                     timerange: Optional[TimeRange] = None) -> pd.DataFrame:
         """
@@ -81,7 +77,6 @@ class HDF5DataHandler(IDataHandler):
         """
         key = self._pair_ohlcv_key(pair, timeframe)
         filename = self._pair_data_filename(self._datadir, pair, timeframe)
-
         if not filename.exists():
             return pd.DataFrame(columns=self._columns)
         where = []
@@ -92,13 +87,13 @@ class HDF5DataHandler(IDataHandler):
                 where.append(f"date < Timestamp({timerange.stopts * 1e9})")
 
         pairdata = pd.read_hdf(filename, key=key, mode="r", where=where)
-
         if list(pairdata.columns) != self._columns:
             raise ValueError("Wrong dataframe format")
         pairdata = pairdata.astype(dtype={'open': 'float', 'high': 'float',
                                           'low': 'float', 'close': 'float', 'volume': 'float'})
         return pairdata
 
+      
     def ohlcv_purge(self, pair: str, timeframe: str) -> bool:
         """
         Remove data for this pair
@@ -112,6 +107,7 @@ class HDF5DataHandler(IDataHandler):
             return True
         return False
 
+      
     def ohlcv_append(self, pair: str, timeframe: str, data: pd.DataFrame) -> None:
         """
         Append data to existing data structures
@@ -121,6 +117,7 @@ class HDF5DataHandler(IDataHandler):
         """
         raise NotImplementedError()
 
+        
     @classmethod
     def trades_get_pairs(cls, datadir: Path) -> List[str]:
         """
@@ -133,6 +130,7 @@ class HDF5DataHandler(IDataHandler):
         # Check if regex found something and only return these results to avoid exceptions.
         return [match[0].replace('_', '/') for match in _tmp if match]
 
+      
     def trades_store(self, pair: str, data: TradeList) -> None:
         """
         Store trades data (list of Dicts) to file
@@ -141,13 +139,13 @@ class HDF5DataHandler(IDataHandler):
                      column sequence as in DEFAULT_TRADES_COLUMNS
         """
         key = self._pair_trades_key(pair)
-
         ds = pd.HDFStore(self._pair_trades_filename(self._datadir, pair),
                          mode='a', complevel=9, complib='blosc')
         ds.put(key, pd.DataFrame(data, columns=DEFAULT_TRADES_COLUMNS),
                format='table', data_columns=['timestamp'])
         ds.close()
 
+        
     def trades_append(self, pair: str, data: TradeList):
         """
         Append data to existing files
@@ -157,6 +155,7 @@ class HDF5DataHandler(IDataHandler):
         """
         raise NotImplementedError()
 
+        
     def _trades_load(self, pair: str, timerange: Optional[TimeRange] = None) -> TradeList:
         """
         Load a pair from h5 file.
@@ -166,7 +165,6 @@ class HDF5DataHandler(IDataHandler):
         """
         key = self._pair_trades_key(pair)
         filename = self._pair_trades_filename(self._datadir, pair)
-
         if not filename.exists():
             return []
         where = []
@@ -175,11 +173,11 @@ class HDF5DataHandler(IDataHandler):
                 where.append(f"timestamp >= {timerange.startts * 1e3}")
             if timerange.stoptype == 'date':
                 where.append(f"timestamp < {timerange.stopts * 1e3}")
-
         trades: pd.DataFrame = pd.read_hdf(filename, key=key, mode="r", where=where)
         trades[['id', 'type']] = trades[['id', 'type']].replace({np.nan: None})
         return trades.values.tolist()
 
+      
     def trades_purge(self, pair: str) -> bool:
         """
         Remove data for this pair
@@ -188,24 +186,27 @@ class HDF5DataHandler(IDataHandler):
         """
         filename = self._pair_trades_filename(self._datadir, pair)
         if filename.exists():
-            filename.unlink()
-            return True
+            filename.unlink(); return True
         return False
-
+      
+      
     @classmethod
     def _pair_ohlcv_key(cls, pair: str, timeframe: str) -> str:
         return f"{pair}/ohlcv/tf_{timeframe}"
 
+      
     @classmethod
     def _pair_trades_key(cls, pair: str) -> str:
         return f"{pair}/trades"
 
+      
     @classmethod
     def _pair_data_filename(cls, datadir: Path, pair: str, timeframe: str) -> Path:
         pair_s = misc.pair_to_filename(pair)
         filename = datadir.joinpath(f'{pair_s}-{timeframe}.h5')
         return filename
 
+      
     @classmethod
     def _pair_trades_filename(cls, datadir: Path, pair: str) -> Path:
         pair_s = misc.pair_to_filename(pair)
