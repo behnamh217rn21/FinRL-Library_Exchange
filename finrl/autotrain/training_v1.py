3821e4c71d6c13b8583954ce34da5990da4869af
behnam heydari <behnamheydari.7.21@gmail.com>
2021-04-21 20:59:17 +0430

    Update training_v1.py

--- finrl/autotrain/training_v1.py
+++ finrl/autotrain/training_v1.py
@@ -81,7 +81,7 @@ def main():
     information_cols = ["close", "macd", "boll_ub", "boll_lb", "rsi_30", "cci_30", "dx_30", 
                         "close_30_sma", "close_60_sma", "log_volume", "change", "daily_variance"]
     env_trade_kwargs = {'initial_amount': initial_amount*500,
-                        'hmax': 100, 
+                        'hmax': 5000, 
                         'cache_indicator_data': True,
                         'daily_information_cols': information_cols,
                         'print_verbosity': 500, 
