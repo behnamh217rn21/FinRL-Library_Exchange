fe65371d66b00c69b26319fd746c2341ee5fab52
behnam heydari <behnamheydari.7.21@gmail.com>
2021-04-25 14:30:41 +0430

    Update training.py

--- finrl/autotrain/training.py
+++ finrl/autotrain/training.py
@@ -82,6 +82,8 @@ def main():
     information_cols = ["close", "macd", "boll_ub", "boll_lb", "rsi_30", "cci_30", "dx_30", \
                         "close_30_sma", "close_60_sma", "log_volume", "change", "daily_variance"]
     env_train_kwargs = {'initial_amount': initial_amount*1000,
+                        'buy_cost_pct': 0,
+                        'sell_cost_pct': 0,
                         'cash_penalty_proportion': 0.2,
                         'hmax': 10, 
                         'cache_indicator_data': True,
@@ -94,6 +96,8 @@ def main():
     
     print("****Build Trade Environment****")
     env_trade_kwargs = {'initial_amount': initial_amount*1000,
+                        'buy_cost_pct': 0,
+                        'sell_cost_pct': 0,
                         'cash_penalty_proportion': 0.2,
                         'hmax': 10,
                         'daily_information_cols': information_cols, 
