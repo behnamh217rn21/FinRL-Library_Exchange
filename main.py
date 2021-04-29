f21b2983f7ae7748b98e3fb99e0d8433a423d79b
behnam heydari <behnamheydari.7.21@gmail.com>
2021-04-29 22:00:35 +0430

    Update main.py

--- main.py
+++ main.py
@@ -22,14 +22,6 @@ def build_parser():
 def main():
     parser = build_parser()
     options = parser.parse_args()
-    if not os.path.exists("./" + config.TRAINED_MODEL_DIR):
-        os.makedirs("./" + config.TRAINED_MODEL_DIR)
-    if not os.path.exists("./" + config.RESULTS_DIR):
-        os.makedirs("./" + config.RESULTS_DIR)
-    if not os.path.exists("./" + config.TENSORBOARD_LOG_DIR):
-        os.makedirs("./" + config.TENSORBOARD_LOG_DIR)
-    if not os.path.exists("./" + config.DATA_SAVE_DIR):
-        os.makedirs("./" + config.DATA_SAVE_DIR)
 
     if options.mode == "train":
         import finrl.autotrain.training
