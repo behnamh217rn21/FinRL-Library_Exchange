a7cce2f0e6f65cea55ba20c8ca390a578f17cec5
behnam heydari <behnamheydari.7.21@gmail.com>
2021-05-01 20:49:36 +0430

    Update training_v1.py

--- finrl/autotrain/training_v1.py
+++ finrl/autotrain/training_v1.py
@@ -87,7 +87,6 @@ def main():
                         'buy_cost_pct': 0,
                         'hmax': 10,
                         'cash_penalty_proportion': 0.2,
-                        'cache_indicator_data': True,
                         'daily_information_cols': information_cols,
                         'print_verbosity': 500, 
                         'discrete_actions': False}
