a33511d0c51ac2788b7c82d0df1d7ecb110120f7
behnam heydari <behnamheydari.7.21@gmail.com>
2021-05-02 20:32:24 +0430

    Update env_stocktrading_stoploss_online.py

--- finrl/env/trade_env/env_stocktrading_stoploss_online.py
+++ finrl/env/trade_env/env_stocktrading_stoploss_online.py
@@ -452,7 +452,8 @@ class StockTradingEnvStopLossOnline(gym.Env):
             path = "/mnt/c/Users/BEHNAMH721AS.RN/AppData/Roaming/MetaQuotes/Terminal/58F16B8C9F18D6DD6A5DAC862FC9CB62/MQL4/Files"
             os.chdir(path)
             order_data = pd.read_csv("OrdersReport.csv", sep=';')
-            os.chdir("mnt/c/Users/BEHNAMH721AS.RN/OneDrive/Desktop/FinRL-Library_Exchange")
+            os.chdir(../../../../../../..)
+            os.chdir("OneDrive/Desktop/FinRL-Library_Exchange")
             swap = 0
             commission = 0
             for i in range(0, len(order_data)):
