0c0a94112404aa62d3a5c9681fd1f1ca2d76c2fc
behnam heydari <behnamheydari.7.21@gmail.com>
2021-05-02 23:45:16 +0430

    Update traders_v1.py

--- finrl/env/dwx_zeromq_connector/strategies/traders_v1.py
+++ finrl/env/dwx_zeromq_connector/strategies/traders_v1.py
@@ -192,6 +192,7 @@ class t_class(DWX_ZMQ_Strategy):
     def _stop_(self):        
         for _t in self._traders:      
             # wait for traders to finish.
+            _t.join() 
             _t.kill() 
         print("\ntraders finished.\n")
                                                             
