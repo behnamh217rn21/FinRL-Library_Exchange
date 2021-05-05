9b386b3ac0ce2b58f3634fdb429dbaf087e5450f
behnam heydari <behnamheydari.7.21@gmail.com>
2021-05-05 22:33:28 +0430

    Update rates_subscriptions_v1.py

--- finrl/env/dwx_zeromq_connector/strategies/rates_subscriptions_v1.py
+++ finrl/env/dwx_zeromq_connector/strategies/rates_subscriptions_v1.py
@@ -146,7 +146,9 @@ class rates_subscriptions(DWX_ZMQ_Strategy):
         self.cnt += 1
         self.data_df.loc[self.cnt, :] = (str(_time), float(_open), float(_high), float(_low), float(_close), int(_tick_vol), int(_spread), int(_real_vol), _topic.split("_")[0], \
                                          float(_macd), float(_boll_ub), float(_boll_lb), float(_rsi_30), float(_cci_30), float(_adx_30), float(_close_30_sma), float(_close_60_sma))
-
+        print("kkkkkkkkkkkkkkkkkkkkkkkk")
+        print(self.cnt)
+        print(len(self._instruments))
         if ((self.cnt+1) % len(self._instruments)) == 0:
             self.data_df.drop(["spread", "real_volume"], axis=1, inplace=True)
             self.data_df["change"] = (self.data_df.close-self.data_df.open)/self.data_df.close
