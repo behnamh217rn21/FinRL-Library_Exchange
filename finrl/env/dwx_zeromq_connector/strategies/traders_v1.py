585a0a9f152af295f1fc7f6612f80ea99b80afdb
behnam heydari <behnamheydari.7.21@gmail.com>
2021-05-06 20:36:13 +0430

    Update traders_v1.py

--- finrl/env/dwx_zeromq_connector/strategies/traders_v1.py
+++ finrl/env/dwx_zeromq_connector/strategies/traders_v1.py
@@ -28,7 +28,7 @@ import pandas as pd
 class t_class(DWX_ZMQ_Strategy):
     def __init__(self, _name="ONLINE_TRADERS",
                  _symbols=['#INTC', '#AAPL'],
-                 _delay=2,
+                 _delay=0.1,
                  _broker_gmt=3,
                  _verbose=False
                  ):
