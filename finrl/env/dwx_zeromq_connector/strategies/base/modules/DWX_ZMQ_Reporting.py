b7892d61614c6d784748d2f682bc1ffdf5d841d1
behnam heydari <behnamheydari.7.21@gmail.com>
2021-05-06 15:12:32 +0430

    Update DWX_ZMQ_Reporting.py

--- finrl/env/dwx_zeromq_connector/strategies/base/modules/DWX_ZMQ_Reporting.py
+++ finrl/env/dwx_zeromq_connector/strategies/base/modules/DWX_ZMQ_Reporting.py
@@ -26,9 +26,10 @@ class DWX_ZMQ_Reporting():
                           _delay=0.1, _wbreak=10):
         # Reset data output
         self._zmq._set_response_(None)
-        
+        print("1111111111111111111111111111")
         # Get open trades from MetaTrader
         self._zmq._DWX_MTX_GET_ALL_OPEN_TRADES_()
+        print("2222222222222222222222222222")
 
         # While loop start time reference            
         _ws = to_datetime('now')
@@ -36,18 +37,25 @@ class DWX_ZMQ_Reporting():
         # While data not received, sleep until timeout
         while self._zmq._valid_response_('zmq') == False:
             sleep(_delay)
+            print("3333333333333333333333")
             if (to_datetime('now') - _ws).total_seconds() > (_delay * _wbreak):
                 break
-        
+        print("444444444444444444444444444")
         # If data received, return DataFrame
         if self._zmq._valid_response_('zmq'):
             _response = self._zmq._get_response_()
-            
+            print("00000000000000000000000000000")
+            print(_response)
             if ('_trades' in _response.keys()
                 and len(_response['_trades']) > 0):
+                print("ttttttttttttttttttttttttttttttttttt")
                 _df = DataFrame(data=_response['_trades'].values(),
                                 index=_response['_trades'].keys())
-                
+                print("yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy")
+                print(_df)
+                print(_response['_trades'].values())
+                print(_response['_trades'].keys())
+                print("lhhhhhhhhhhhhhhhhhhhhhh")
                 return _df[_df['_comment'] == _trader]
             
         # Default
