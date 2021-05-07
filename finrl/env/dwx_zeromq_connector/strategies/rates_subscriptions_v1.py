bd14592d51cfe59124e20762ffc5865727908002
behnam heydari <behnamheydari.7.21@gmail.com>
2021-05-07 14:12:56 +0430

    Update rates_subscriptions_v1.py

--- finrl/env/dwx_zeromq_connector/strategies/rates_subscriptions_v1.py
+++ finrl/env/dwx_zeromq_connector/strategies/rates_subscriptions_v1.py
@@ -154,6 +154,7 @@ class rates_subscriptions(DWX_ZMQ_Strategy):
         try:
             if (self.cnt+1) % len(self._instruments) == 0:
                 assert len(self.data_df) == len(self._instruments)
+                self.cnt = -1
                 if f_time != str(_time):
                     self.data_df.drop(["spread", "real_volume"], axis=1, inplace=True)
                     fe = FeatureEngineer(use_technical_indicator=False,
@@ -172,7 +173,7 @@ class rates_subscriptions(DWX_ZMQ_Strategy):
                     with open('./finrl/marketdata/f_time.txt', 'w') as f2:
                         f2.write('%s' % str(_time))
                     
-                    # finishes (removes all subscriptions)  
+                    # finishes (removes all subscriptions)
                     self.stop()
                     
         except AssertionError:
