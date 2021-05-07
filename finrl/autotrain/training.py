0a8875cb95f39495d5158db0c228cdd0a50f217c
behnam heydari <behnamheydari.7.21@gmail.com>
2021-05-07 23:19:42 +0430

    Update training.py

--- finrl/autotrain/training.py
+++ finrl/autotrain/training.py
@@ -1,14 +1,9 @@
 import pandas as pd
 import numpy as np
-import matplotlib
-import matplotlib.pyplot as plt
-matplotlib.use("Agg")
-
-import warnings
-warnings.filterwarnings('ignore')
 
 #############################################################################
-from sklearn import preprocessing
+import warnings
+warnings.filterwarnings('ignore')
 import datetime
 
 #############################################################################
