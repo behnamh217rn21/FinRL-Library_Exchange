4b6fecea39065aeab70041f5b346fd10c1065ac5
behnam heydari <behnamheydari.7.21@gmail.com>
2021-05-08 01:00:07 +0430

    Update env_stocktrading_stoploss_online.py

--- finrl/env/trade_env/env_stocktrading_stoploss_online.py
+++ finrl/env/trade_env/env_stocktrading_stoploss_online.py
@@ -197,6 +197,11 @@ class StockTradingEnvStopLossOnline(gym.Env):
     
             
     def get_date_vector(self, date, cols=None):
+        cwd = os.getcwd()
+        if cwd != "/mnt/c/Users/BEHNAMH721AS.RN/OneDrive/Desktop/FinRL-Library_Exchange":
+            path = "/mnt/c/Users/BEHNAMH721AS.RN/OneDrive/Desktop/FinRL-Library_Exchange"
+            os.chdir(path)
+            
         if cols is None:
             cols = self.daily_information_cols
         
@@ -217,11 +222,6 @@ class StockTradingEnvStopLossOnline(gym.Env):
                 #sleep_t = (fetch_t - now_t).total_seconds()
             #print("sleep for {} second".format(sleep_t))
             #sleep(sleep_t)
-
-            cwd = os.getcwd()
-            if cwd != "/mnt/c/Users/BEHNAMH721AS.RN/OneDrive/Desktop/FinRL-Library_Exchange":
-                path = "/mnt/c/Users/BEHNAMH721AS.RN/OneDrive/Desktop/FinRL-Library_Exchange"
-                os.chdir(path)
                 
             print("****Start Fetching Data (rates subscriptions process)****")
             self.rates_subscriptions(self._symbols,)
