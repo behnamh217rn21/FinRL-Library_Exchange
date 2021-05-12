536b05b833b383304130e9bafc02bb857949458e
behnam heydari <behnamheydari.7.21@gmail.com>
2021-05-12 14:49:59 +0430

    Update env_stocktrading_stoploss_online.py

--- finrl/env/trade_env/env_stocktrading_stoploss_online.py
+++ finrl/env/trade_env/env_stocktrading_stoploss_online.py
@@ -440,14 +440,14 @@ class StockTradingEnvStopLossOnline(gym.Env):
 
             # compute our proceeds from sells, and add to cash
             sells = -np.clip(actions, -np.inf, 0)
-            sells = np.round(sells, 2)
+            sells = np.round(sells, 5)
             proceeds = np.dot(sells*100000, closings) / self.Leverage
             costs = proceeds * self.sell_cost_pct
             coh = begin_cash + proceeds
 
             # compute the cost of our buys
             buys = np.clip(actions, 0, np.inf)
-            buys = np.round(buys, 2)
+            buys = np.round(buys, 5)
             spend = np.dot(buys*100000, closings) / self.Leverage
             costs += spend * self.buy_cost_pct
             
@@ -499,8 +499,8 @@ class StockTradingEnvStopLossOnline(gym.Env):
             #coh = coh - spend - costs - swap - commission
             
             # update our holdings
-            actions = np.round(actions, 2)
-            holdings = np.round(holdings, 2)
+            actions = np.round(actions, 5)
+            holdings = np.round(holdings, 5)
             holdings_updated = holdings + actions
 
             # Update average buy price
