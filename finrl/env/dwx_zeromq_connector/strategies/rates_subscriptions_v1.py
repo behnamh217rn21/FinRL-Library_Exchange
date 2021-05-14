c476515045fb081d82be38408ff0559744055cb4
behnam heydari <behnamheydari.7.21@gmail.com>
2021-05-14 21:04:00 +0430

    Update rates_subscriptions_v1.py

--- finrl/env/dwx_zeromq_connector/strategies/rates_subscriptions_v1.py
+++ finrl/env/dwx_zeromq_connector/strategies/rates_subscriptions_v1.py
@@ -159,6 +159,8 @@ class rates_subscriptions(DWX_ZMQ_Strategy):
 
         try:
             if (self.cnt+1) % len(self._instruments) == 0:
+                print("1111111111111111111")
+                print(self.data_df)
                 assert len(self.data_df) == len(self._instruments) 
                 assert self.data_df.index[0] == 0
                 self.cnt = -1
