8dfa2ddeac7521b421173b9320c8bc81a48a519d
behnam heydari <behnamheydari.7.21@gmail.com>
2021-05-14 17:51:40 +0430

    Update env_stocktrading_stoploss_FX.py

--- finrl/env/env_stocktrading_stoploss_FX.py
+++ finrl/env/env_stocktrading_stoploss_FX.py
@@ -411,10 +411,6 @@ class StockTradingEnvStopLoss(gym.Env):
             coh = coh - spend - costs
 
             # update our holdings
-            holdings = list(map(lambda x: round(x, ndigits=5), holdings))
-            holdings = np.asarray(holdings)
-            actions = list(map(lambda x: round(x, ndigits=5), actions))
-            actions = np.asarray(actions)
             holdings_updated = holdings + actions
             
             # Update average buy price
