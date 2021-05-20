e99fb02b3af815ef781c3d6e2f2ef4bc29d332a5
behnam heydari <behnamheydari.7.21@gmail.com>
2021-05-20 15:24:08 +0430

    Update env_stocktrading_stoploss_online.py

--- finrl/env/trade_env/env_stocktrading_stoploss_online.py
+++ finrl/env/trade_env/env_stocktrading_stoploss_online.py
@@ -418,7 +418,8 @@ class StockTradingEnvStopLossOnline(gym.Env):
                 #actions = np.where(closings > 0, actions / closings, 0)
                 actions = list(map(lambda x: round(x, ndigits=2), actions))
                 actions = np.asarray(actions)
-                
+                print("actions:\n{}".format(actions))
+
             # clip actions so we can't sell more assets than we hold
             actions = np.maximum(actions, -np.array(holdings))
             
