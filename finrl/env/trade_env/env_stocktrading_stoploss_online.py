7fcd100ee6578ed365982bbb92895ea2dcdb262d
behnam heydari <behnamheydari.7.21@gmail.com>
2021-05-20 16:53:27 +0430

    Update env_stocktrading_stoploss_online.py

--- finrl/env/trade_env/env_stocktrading_stoploss_online.py
+++ finrl/env/trade_env/env_stocktrading_stoploss_online.py
@@ -418,8 +418,9 @@ class StockTradingEnvStopLossOnline(gym.Env):
                 #actions = np.where(closings > 0, actions / closings, 0)
                 actions = list(map(lambda x: round(x, ndigits=2), actions))
                 actions = np.asarray(actions)
+                print("actions [ProCent]:\n{}".format(actions))
                 actions = np.where(actions>=0.1, actions, 0) # ProCent_accounts
-                print("actions:\n{}".format(actions))
+                print("actions [Normal]:\n{}".format(actions))
 
             # clip actions so we can't sell more assets than we hold
             actions = np.maximum(actions, -np.array(holdings))
