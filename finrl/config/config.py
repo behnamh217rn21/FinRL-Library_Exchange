185f9fba0b46a9a5e7a7536fbfec36c39d519bfc
behnam heydari <behnamheydari.7.21@gmail.com>
2021-05-20 20:52:01 +0430

    Update config.py

--- finrl/config/config.py
+++ finrl/config/config.py
@@ -19,10 +19,10 @@ TENSORBOARD_LOG_DIR = f"finrl/tensorboard_log"
 RESULTS_DIR = f"finrl/results"
 
 ## time_fmt = '%Y-%m-%d %H:%M:00'
-START_DATE = "2021-01-15"; END_DATE = "2021-05-18"
-START_TRADE_DATE = "2021-04-04"
-S_SPLIT = "2021-01-15 14:00:00"; F_SPLIT = "2021-05-18 19:00:00"
-T_SPLIT = "2021-04-04 00:00:00"
+START_DATE = "2021-01-15"; END_DATE = "2021-05-20"
+START_TRADE_DATE = "2021-04-20"
+S_SPLIT = "2021-01-15 14:00:00"; F_SPLIT = "2021-05-20 18:00:00"
+T_SPLIT = "2021-04-20 00:00:00"
 
 ## dataset default columns
 DEFAULT_DATA_COLUMNS = ["date", "tic", "close"]
