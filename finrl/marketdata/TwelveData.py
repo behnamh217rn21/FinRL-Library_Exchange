ab888f9b553188528c17bc317a1e29be9e463686
behnam heydari <behnamheydari.7.21@gmail.com>
2021-05-23 23:33:11 +0430

    Update TwelveData.py

--- finrl/marketdata/TwelveData.py
+++ finrl/marketdata/TwelveData.py
@@ -3,10 +3,13 @@ from time import sleep
 import pandas as pd
 from pandas import to_datetime
 
+from finrl.config import config
+
+
 # Initialize client - apikey parameter is requiered
 td = TDClient(apikey="772d7db2959540db9975d00276a8f0b2")
 
-with open("symbols.txt", "r") as file:
+with open("./" + config.DATA_SAVE_DIR + "/symbols.txt", "r") as file:
     _symbols = eval(file.readline())
 _symbols_i1 = []
 for i in range(0, len(_symbols)):
@@ -18,7 +21,7 @@ for symbol in(_symbols_i1):
     # Construct the necessary time serie
     ts = td.time_series(symbol=_tic,
                         interval="1h",
-    	                  outputsize=5000)
+    	                outputsize=5000)
     print(symbol)
     df_t = ts.with_bbands().with_ema().with_macd().with_cci().with_atr().with_rsi().with_adx()
     sleep(60)
@@ -33,5 +36,5 @@ df.reset_index(inplace=True)
 df["datetime"] = pd.to_datetime(df["datetime"])
 df = df.sort_values(by=['datetime','tic']).reset_index(drop=True)
 
-df.to_csv("data.csv")
+df.to_csv("./" + config.DATASET_DIR + "/data.csv")
 print(df)
