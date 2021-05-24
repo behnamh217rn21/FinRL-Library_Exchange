77282aee635c3d5fdaf6e5aad4a2fa696c23cc1c
behnam heydari <behnamheydari.7.21@gmail.com>
2021-05-24 21:55:22 +0430

    Update training_v1.py

--- finrl/autotrain/training_v1.py
+++ finrl/autotrain/training_v1.py
@@ -97,7 +97,7 @@ def main():
     Leverage = in_L.loc[0, 'Leverage']
     print("Leverage : {}".format(Leverage))
 
-    information_cols = ["close", "upper_band", "lower_band", "ema", "macd_signal", "macd_hist", "cci", "atr", "rsi", "adx"]
+    information_cols = ["close", "upper_band", "lower_band", "ema", "macd_signal", "macd_hist", "cci", "rsi", "adx", "slow_k", "slow_d"]
     env_train_kwargs = {'initial_amount': initial_amount_train*float(Leverage),
                         'leverage': Leverage,
                         'sell_cost_pct': 0,
