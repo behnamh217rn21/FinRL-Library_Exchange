e7b04a6978302c264b8d710f0d39defd1a510373
behnam heydari <behnamheydari.7.21@gmail.com>
2021-05-24 12:54:41 +0430

    Update trading_v1_2.py

--- finrl/autotrain/trading_v1_2.py
+++ finrl/autotrain/trading_v1_2.py
@@ -31,8 +31,7 @@ def main():
     #print(StockTradingEnvStopLoss_online.__doc__)
 
     print("****Build Trade Environment****")
-    information_cols = ["close", "macd", "boll_ub", "boll_lb", "rsi_30", "cci_30", "dx_30",
-                        "close_30_sma", "close_60_sma", "log_volume", "change", "daily_variance"]
+    information_cols = ["close", "upper_band", "lower_band", "ema", "macd_signal", "macd_hist", "cci", "atr", "rsi", "adx"]
     env_trade_kwargs = {'sell_cost_pct': 0,
                         'buy_cost_pct': 0,
                         'hmax': 1,
